d5587a4ed1b864da96dc7e09baad86750332a578
maxsimmonds <maxsimmonds1337@gmail.com>
2020-06-23 07:15:01 +0100

    Still not working

--- top.vhd
+++ top.vhd
@@ -9,10 +9,10 @@ use IEEE.numeric_std.all;               -- IEEE library for the unsigned type an
 entity TOP is
 	port(
 		--inputs
-		clk_in_top		:	in	STD_LOGIC	:=	'0';		-- Clock in  (25.175 MHz) on P26
+		clk_in_top		:	in	STD_LOGIC;		-- Clock in  (25.175 MHz) on P26
 
 		--outputs
-		HS_top			:	out	STD_LOGIC	:=	'0';		-- Horiontal sync
+		HS_top			:	out	STD_LOGIC;		-- Horiontal sync
 		VS_top			:	out	STD_LOGIC;		-- Virticle sync
 		R_out_top		:	out	STD_LOGIC;		-- Red signal out
 		G_out_top		:	out	STD_LOGIC;		-- Green signal out
@@ -20,16 +20,26 @@ entity TOP is
 	);
 end TOP;
 
---functionality
-architecture BEHAVIORAL of TOP is
+--functionality	
+architecture BEHAVIORAL of TOP is	
+	
+	signal r_HS_top : STD_LOGIC := '0';
+	signal r_VS_top : STD_LOGIC := '0';
+	signal r_R_out_top : STD_LOGIC := '0';
+	signal r_G_out_top : STD_LOGIC := '0';
+	signal r_B_out_top : STD_LOGIC := '0';
+	
 	begin
-
-	clk_out: process(clk_in_top) is
-		begin
-			if(rising_edge(clk_in_top)) then
-		
-				--combitorial logic
-				HS_top <= clk_in_top;	-- out the clock
-			end if;
-		end process clk_out;
+	
+	CLK: process(clk_in_top) is
+	begin
+		r_HS_top <= not r_HS_top;
+	end process CLK;
+	
+	HS_top <= r_HS_top;
+	VS_top <= r_VS_top;
+	R_out_top <= r_R_out_top;
+	G_out_top <= r_G_out_top;
+	B_out_top <= r_B_out_top;
+	
 end BEHAVIORAL;
