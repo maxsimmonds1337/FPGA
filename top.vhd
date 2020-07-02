b4e63418ef705eb15fc83c29f50699c70fd26055
maxsimmonds <maxsimmonds1337@gmail.com>
2020-07-02 19:03:42 +0100

    updated code

--- top.vhd
+++ top.vhd
@@ -1,79 +1,55 @@
--- FPGA projects using VHDL/ VHDL 
--- fpga4student.com
--- VHDL code for D Flip FLop
--- VHDL code for Rising edge D flip flop with Asynchronous Reset high
-Library IEEE;
-USE IEEE.Std_logic_1164.all;
+ --library declaration
+library IEEE;
 
-entity RisingEdge_DFlipFlop_AsyncResetHigh is 
-   port(
-      Q : out std_logic;    
-      Clk :in std_logic;  
-   sync_reset: in std_logic;  
-      D :in  std_logic    
-   );
-end RisingEdge_DFlipFlop_AsyncResetHigh;
-architecture Behavioral of RisingEdge_DFlipFlop_AsyncResetHigh is  
-begin  
-
-	Q <= Clk;
--- process(Clk,sync_reset)
--- begin 
---     if(sync_reset='1') then 
---   Q <= '0';
---     elsif(rising_edge(Clk)) then
---   Q <= '1'; 
---		else
---	Q <= '0';
---  end if;      
--- end process;  
-end Behavioral; 
+use IEEE.std_logic_1164.all;            -- basic IEEE library
+use IEEE.numeric_std.all;               -- IEEE library for the unsigned type and various arithmetic operators
 
 
+-- Top level entity - defines the IO
+entity TOP is
+	port(
+		--inputs
+		pixel_clk		:	in	STD_LOGIC;		-- Clock in  (25.175 MHz) on P26
+		--reset			:	in	STD_LOGIC;		-- SW5 (J17)
 
--- --library declaration
---library IEEE;
---
---use IEEE.std_logic_1164.all;            -- basic IEEE library
---use IEEE.numeric_std.all;               -- IEEE library for the unsigned type and various arithmetic operators
---
---
----- Top level entity - defines the IO
---entity TOP is
---	port(
---		--inputs
---		clk_in_top		:	in	STD_LOGIC;		-- Clock in  (25.175 MHz) on P26
---		reset				:	in	STD_LOGIC;
---
---		--outputs
---		HS_top			:	out	STD_LOGIC		-- Horiontal sync
---		--VS_top			:	out	STD_LOGIC;		-- Virticle sync
---		--R_out_top		:	out	STD_LOGIC;		-- Red signal out
---		--G_out_top		:	out	STD_LOGIC;		-- Green signal out
---		--B_out_top		:	out	STD_LOGIC		-- Blue signal out
---	);
---end TOP;
---
-----functionality	
---architecture BEHAVIORAL of TOP is	
---	
---	--signal r_HS_top 		: 	STD_LOGIC := '0';
---	--signal r_VS_top 		: 	STD_LOGIC := '0';
---	--signal r_R_out_top 	:	STD_LOGIC := '0';
---	--signal r_G_out_top 	:	STD_LOGIC := '0';
---	--signal r_B_out_top 	:	STD_LOGIC := '0';
---
---	begin
---	
---	process(clk_in_top, reset) is
---	begin
---		if reset = '1' then
---			HS_top <= '0';
---		elsif (rising_edge(clk_in_top) ) then 
---			HS_top <= '1';
---		end if;
---	end process;
---	
---	--HS_top <= r_HS_top;
---	
---end BEHAVIORAL;
+		--outputs
+		HS_top			:	out	STD_LOGIC		-- Horiontal sync
+		--VS_top			:	out	STD_LOGIC;		-- Virticle sync
+		--R_out_top		:	out	STD_LOGIC;		-- Red signal out
+		--G_out_top		:	out	STD_LOGIC;		-- Green signal out
+		--B_out_top		:	out	STD_LOGIC		-- Blue signal out
+	);
+end TOP;
+
+--functionality	
+architecture BEHAVIORAL of TOP is	
+
+	--signal r_HS_top 		: 	STD_LOGIC := '0';
+	--signal r_VS_top 		: 	STD_LOGIC := '0';
+	--signal r_R_out_top 	:	STD_LOGIC := '0';
+	--signal r_G_out_top 	:	STD_LOGIC := '0';
+	--signal r_B_out_top 	:	STD_LOGIC := '0';
+
+	component VGA_640x480 is
+		port(
+			--inputs
+			pixel_clk	:	in	STD_LOGIC;		-- pixel clock, this is at 25.175MH
+
+			--outputs
+			hsync		:	out	STD_LOGIC		-- horizontal sync
+		);
+	end component;
+
+begin
+
+	VGA_p: VGA_640x480
+			PORT MAP (
+				-- inputs
+				pixel_clk => pixel_clk,
+
+				--outputs
+				hsync => HS_top
+			);
+	
+
+end BEHAVIORAL;
--- top_tb.vhd
+++ top_tb.vhd
@@ -14,6 +14,7 @@ architecture BEHAVIORAL of top_tb is
 
 	constant C_PERIOD	:	Time		:=	39.72194638 ns;				-- 25.175MHz
 	signal clk_tb		: 	STD_LOGIC	:=	'0'; 						-- clock signal, default 0
+	signal reset_tb		: 	STD_LOGIC	:=	'0'; 						-- clock signal, default 0
 
 	signal HS_top_tb		: 	STD_LOGIC	:=	'0'; 						-- clock signal, default 0
 	signal VS_top_tb		: 	STD_LOGIC	:=	'0'; 						-- clock signal, default 0
@@ -24,14 +25,15 @@ architecture BEHAVIORAL of top_tb is
 	component top is
 		port(
 			--inputs
-			clk_in_top		:	in	STD_LOGIC;		-- Clock in  (25.175 MHz) on P26
+			pixel_clk		:	in	STD_LOGIC;		-- Clock in  (25.175 MHz) on P26
+			--reset			:	in	STD_LOGIC;		-- SW5 (J17)
 
 			--outputs
-			HS_top			:	out	STD_LOGIC;		-- Horiontal sync
-			VS_top			:	out	STD_LOGIC;		-- Virticle sync
-			R_out_top		:	out	STD_LOGIC;		-- Red signal out
-			G_out_top		:	out	STD_LOGIC;		-- Green signal out
-			B_out_top		:	out	STD_LOGIC		-- Blue signal out
+			HS_top			:	out	STD_LOGIC		-- Horiontal sync
+			--VS_top			:	out	STD_LOGIC;		-- Virticle sync
+			--R_out_top		:	out	STD_LOGIC;		-- Red signal out
+			--G_out_top		:	out	STD_LOGIC;		-- Green signal out
+			--B_out_top		:	out	STD_LOGIC		-- Blue signal out
 		);
 	end component top;
  
@@ -40,13 +42,14 @@ architecture BEHAVIORAL of top_tb is
 
 		TOP_p: top
 			PORT MAP (
-				clk_in_top => clk_tb,
+				pixel_clk => clk_tb,
+				--reset => reset_tb,
 
-				HS_top => HS_top_tb,
-				VS_top => VS_top_tb,
-				R_out_top => R_out_top_tb,
-				G_out_top => G_out_top_tb,
-				B_out_top => B_out_top_tb
+				HS_top => HS_top_tb
+				--VS_top => VS_top_tb,
+				--R_out_top => R_out_top_tb,
+				--G_out_top => G_out_top_tb,
+				--B_out_top => B_out_top_tb
 			);
 
 end BEHAVIORAL;
